93beb473869108c1c3f0e5a2a7d23fe42ff6bbab
crimsoncritter <47731322+crimsoncritter@users.noreply.github.com>
2020-04-10 12:55:49 -0400

    Added playlist view and button actions
    Created playlist view using a Stack Pane and began actions for buttons.

--- Octave_Player/src/octave_player/OctaveView.java
+++ Octave_Player/src/octave_player/OctaveView.java
@@ -14,6 +14,7 @@ import octave_player.Observer;
 
 // Containers
 import java.util.ArrayList;
+import java.util.List;
 
 //JavaFX imports
 import javafx.scene.layout.GridPane;
@@ -31,6 +32,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaView;
+import javafx.geometry.Insets;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
 
 
 /**
@@ -73,12 +78,29 @@ public class OctaveView implements Observer {
         // Once Observer and Observable interfaces are written, the constructor
         // will need to bind itself as an observer to each of the observables
         // Observables: The queue, and the playlists
+    
+    /********************Currently Playing**********************/     
+        Label current = new Label("Now Playing:");
+        current.setFont(new Font(25));
         
+        HBox c = new HBox(current);
+        c.setAlignment(Pos.CENTER);
+
+    /********************Playlist View**********************/        
+        StackPane playlist = new StackPane();
+        playlist.setAlignment(Pos.CENTER);
+        playlist.setPadding(new Insets(20));
+        
+    /********************Control Bar**********************/
         //Skip Forward Button
         f = new Button();
         f.setGraphic(iv4);
         iv4.setFitHeight(60);
         iv4.setFitWidth(60);
+        
+        f.setOnAction(e -> {
+            
+        });
         
         //Play Button
         pl = new Button();
@@ -86,11 +108,19 @@ public class OctaveView implements Observer {
         iv.setFitHeight(60);
         iv.setFitWidth(60);
         
+        pl.setOnAction(e -> {
+            
+        });
+        
         //Pause Button
         pa = new Button();
         pa.setGraphic(iv2);
         iv2.setFitHeight(60);
         iv2.setFitWidth(60);
+        
+        pa.setOnAction(e -> {
+            
+        });
         
         //Stop Button
         s = new Button();
@@ -98,11 +128,19 @@ public class OctaveView implements Observer {
         iv6.setFitHeight(60);
         iv6.setFitWidth(60);
         
+        s.setOnAction(e -> {
+            
+        });
+        
         //Skip to Previous Button
         p = new Button();
         p.setGraphic(iv5);
         iv5.setFitHeight(60);
         iv5.setFitWidth(60);
+        
+        p.setOnAction(e -> {
+            
+        });
         
         //Seek Slider
         seek = new Slider();
@@ -149,7 +187,10 @@ public class OctaveView implements Observer {
         
         BorderPane root = new BorderPane();
         root.setStyle("-fx-background-color: lightgrey");
+        root.setPadding(new Insets(20));
         root.setBottom(gp);
+        root.setTop(c);
+        root.setCenter(playlist);
         
         Scene mainScene = new Scene(root,1000,800);
         this.stage.setScene(mainScene);
